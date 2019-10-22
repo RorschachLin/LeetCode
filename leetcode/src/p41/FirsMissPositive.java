7142dc0130f40b0810d6e71542ed5fb8ae19bd5a
rorschach <294528248@qq.com>
2019-10-23 00:21:17 +0800

    first missing positive using bucket sort

---
+++ leetcode/src/p41/FirsMissPositive.java
@@ -0,0 +1,39 @@
+package p41;
+
+public class FirsMissPositive {
+
+	public static int firstMissingPositive(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			while (nums[i] <= nums.length && nums[i] > 0 && nums[nums[i] - 1] != nums[i]) {
+				swap(nums, nums[i] - 1, i);
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != i + 1) {
+				return i + 1;
+			}
+		}
+        return nums.length + 1;
+    }
+	
+	// O(1) in space
+	public static void swap(int[] nums, int index1, int index2) {
+		if (index1 == index2) {
+			return;
+		}
+		nums[index1] = nums[index1] ^ nums[index2];
+		nums[index2] = nums[index1] ^ nums[index2];
+		nums[index1] = nums[index1] ^ nums[index2];
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {-1, 3, 1, 4};
+		
+		int result = FirsMissPositive.firstMissingPositive(nums);
+		System.out.println(result);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+
+}
